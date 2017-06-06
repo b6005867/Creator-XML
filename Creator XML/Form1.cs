d1e38d1bd7837c493b7f8691a2bfcd607cf768b3
b6005867 <b6005867@my.shu.ac.uk>
2017-06-06 16:36:39 +0300

    Measurements
    Added measurements which coincide with the article codes. they are stored in hidden listboxes.

--- Creator XML/Form1.cs
+++ Creator XML/Form1.cs
@@ -16,17 +16,47 @@ namespace Creator_XML
         StreamReader reader;
         StreamWriter write;
         string applicationPath = Directory.GetCurrentDirectory() + "\\";
+        ListBox lbx_codes = new ListBox();
+        ListBox lbx_measurements = new ListBox();
+        StreamReader readMeasurements;
 
 
         public Form1()
         {
             InitializeComponent();
+            read_measurements();
+        }
+
+        private void read_measurements()
+        {
+            int index = 1;
+            readMeasurements = File.OpenText(applicationPath + "articole.txt");
+            string line = readMeasurements.ReadLine();
+            while (line != null)
+            {
+                string[] words = line.Split('	');
+                foreach (string word in words)
+                {
+                    if (index % 2 != 0)
+                    {
+                        lbx_codes.Items.Add(word);
+                    }
+                    else
+                    {
+                        lbx_measurements.Items.Add(word);
+                    }
+                    index++;
+                }
+                line = readMeasurements.ReadLine();
+            }
+
         }
 
         private void add_to_listboxes(string article, int quantity)
         {
             lbx_article.Items.Add(article);
             lbx_quantity.Items.Add(quantity);
+            
         }
 
         private void add_line(StreamWriter write, string article, string quantity, int number)
@@ -39,7 +69,6 @@ namespace Creator_XML
             string filePath = applicationPath + "file.xml";
             
             File.Delete(filePath);
-            //write = File.CreateText(filePath);
             write = File.CreateText(filePath);
             write.Write("<?xml version=\"1.0\"?>" + "<Comfurn>" + "<Version>1</Version>" + "<Copyright>2005 - ValWeb</Copyright>" + "<Id>25425</Id>" + "<Nr>25425</Nr>" + "<Data>20.12.2016</Data>");
             int numberOfItems = lbx_article.Items.Count;
